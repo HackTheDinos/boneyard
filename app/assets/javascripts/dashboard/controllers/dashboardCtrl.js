6e6494ac76939e9faad802c7288afeb6d64d254e
Jan Rubio <jan.rubio@opower.com>
2015-11-22 12:10:59 -0500

    Supporting fake queries

--- app/assets/javascripts/dashboard/controllers/dashboardCtrl.js
+++ app/assets/javascripts/dashboard/controllers/dashboardCtrl.js
@@ -16,16 +16,40 @@ angular.module('boneyard')
             return false;
         });
         $scope.rowSelected = false;
+        $scope.form = {};
         $scope.selectedSpecimen;
+        $scope.searchQuery = {};
 
         $scope.filter = function() {
+            var parameters = {};
+            appendParameter(parameters, 'name');
+            appendParameter(parameters, 'scientific_name');
+            appendParameter(parameters, 'common_name');
+            appendParameter(parameters, 'bone_type');
+            appendParameter(parameters, 'museum');
+
+            $scope.searchQuery = _.cloneDeep(parameters);
             $('.search-form--advanced').fadeOut(100);
-            search();
+            search(parameters);
         };
+
+        function appendParameter(object, key) {
+            if (!_.isEmpty(_.trim($scope.form[key]))) {
+                object[key] = $scope.form[key];
+            }
+        }
 
         $scope.quickSearch = function(keyEvent) {
             if (keyEvent.which === 13) {
-                search();
+                var parameters = {};
+                 appendParameter(parameters, 'name');
+                 appendParameter(parameters, 'scientific_name');
+                 appendParameter(parameters, 'common_name');
+                 appendParameter(parameters, 'bone_type');
+                 appendParameter(parameters, 'museum');
+
+                 $scope.searchQuery = _.cloneDeep(parameters);
+                search(parameters);
             }
         };
 
@@ -130,9 +154,15 @@ angular.module('boneyard')
                 .all(parameters)
                 .then(function(allSpecimens) {
                     var specimenList = [];
-                    _.times(200, function() {
+                    var times = 200;
+                    if (_.includes(_.keys($scope.searchQuery), 'name')) {
+                        times = 1;
+                    }
+
+                    _.times(times, function() {
                         specimenList.push(
-                            new Specimen({
+                            new Specimen(_.defaults(_.cloneDeep($scope.searchQuery), {
+                                created_at: _.random(1, 28) + ' ' + _.sample(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']) + ' ' + _.random(1950, 2014),
                                 name: $scope.fake('specimenMuseums') + '-' + _.random(1000) + '' + _.random(999),
                                 scientific_name: $scope.fake('scientificName'),
                                 common_name: $scope.fake('commonName'),
@@ -144,8 +174,9 @@ angular.module('boneyard')
                                 calib_skip: _.random(1, 5),
                                 detector_timing_value: _.random(200.0, 500.0),
                                 xray_current: _.random(100, 240),
-                                xray_voltage: _.random(100, 240)
-                            })
+                                xray_voltage: _.random(100, 240),
+                                author: $scope.fake('doctor')
+                            }))
                         );
                     });
 
@@ -264,6 +295,38 @@ angular.module('boneyard')
                 ],
                 boneType: [
                     'Basicranium','Braincase','Cervical vertebra 3','Cervical vertebra 5','Cervical vertebra 6','Dorsal vertebra','Left angular','Left dentary','Left maxilla','Left postorbital','Left quadrate','Left surangular','Right frontal','Right jugal','Right orbitosphenoid','Right postorbital','Right premaxilla','Braincase','Forelimb','Rostrum','Skull'
+                ],
+                doctor: [
+                    'Kimberlee Liddell',
+                    'Taunya Levay',
+                    'Brook Sotomayor',
+                    'Maryln Smartt',
+                    'Shavonne Rulison',
+                    'Ambrose Melugin',
+                    'Kirby Briggs',
+                    'Mack Birchfield',
+                    'Sang Willmore',
+                    'Patsy Saylor',
+                    'Margene Rathburn',
+                    'Akilah Harewood',
+                    'Laverna Stracener',
+                    'Nanci Shahan',
+                    'Eda Chow',
+                    'Tasia Vandermeulen',
+                    'Dortha Tiernan',
+                    'Cristine Chesley',
+                    'Kaleigh Jardine',
+                    'Juan Sholar',
+                    'Garth Swain',
+                    'Elfreda Padmore',
+                    'Gail Pinelli',
+                    'Demarcus Damm',
+                    'Lenora Driggers',
+                    'Elaina Shackelford',
+                    'Francisca Cromartie',
+                    'Raisa Yearout',
+                    'Everette Moen',
+                    'Stuart Riegel'
                 ],
                 museum: [
                     'American Museum of Natural History','Badlands National Park','Bat Conservation International','Bath Royal Literary and Scientific Institution','Beijing Natural History Museum','Bell Museum of Natural History','Bishop Museum','Brigham Young University Earth Science Museum','California Academy of Sciences','California Institute of Technology','Carnegie Museum of Natural History','Chelonian Research Institute','Cornell University Division of Biological Sciences','Cornell University Museum of Vertebrates','Dinosaur National Monument','Duke University Primate Center','Field Museum of Natural History, Chicago','Finnish Museum of Natural History','Florida Museum of Natural History','Forest Research Institute Malaysia','Geiseltal Museum','Grant Museum of Zoology and Comparative Anatomy','Houston Museum of Natural Science','Illinois State Museum','Institute of Geology, Mongolian Academy of Science','Institute of Geology, Ulaan Baatar, Mongolia','Institute of Vertebrate Paleontology and Paleoanth','Instituto de Biologia, Universidad Nacional Autóno','Instituto Nacional de Pesquisa da Amazonia','Los Angeles County Museum of Natural History','Louisiana State University Museum of Natural Scien','Massachusetts Museum of Natural History','Mongolian Academy of Sciences','Musée d\'Histoire Naturelle de Miguasha','Musée des Dinosaures, Espéraza, France','Musée National du Niger','Museo Argentino de Ciencias Naturales, Buenos Aire','Museo de Ciencias Naturales, Universidad Nacional','Museo de Historia natural de Cochabamba','Museo de La Plata, Argentina','Museo de Zoología, Facultad de Ciencias, U. N. A.','Museu de Zoologia da Universidade de São Paulo','Museu Historic Municipal de Novelda','Museu Nacional, Universidade Federal do Rio de Jan','Museum für Naturkunde Berlin','Muséum National d\'Histoire Naturelle','Museum of Comparative Zoology, Harvard University','Museum of Northern Arizona','Museum Victoria','National Geographic Society Explorers Hall','National Museum of Natural History','National Park Service','North Carolina Museum of Natural Sciences','Ohio University College of Osteopathic Medicine','Old Dominion University','Private Collection','Queensland Museum','Riken Bioresource Center, Tsukuba, Ibaraki, Japan','Royal Botanic Gardens, Kew, United Kingdom','Royal Ontario Museum','Royal Tyrrell Museum of Palaeontology','Sam Noble Oklahoma Museum of Natural History','San Diego Natural History Museum','San Diego Society of Natural History','Scripps Institution of Oceanography','Shuler Museum of Paleontology','South African Institute for Aquatic Biodiversity','South Dakota School of Mines and Technology','Southwest Foundation for Biomedical Research','Texas Cooperative Wildlife Collection','Texas Memorial Museum','The Academy of Natural Sciences of Philadelphia','The Natural History Museum (London)','Tulane University Museum of Natural History','Universidad Nacional Autonoma de Mexico','Université d\'Antananarivo','University College London','University of Alabama Ichthyological Collection','University of Antananarivo','University of Arizona','University of Calgary','University of California Museum of Paleontology','University of California Museum of Vertebrate Zool','University of California, Los Angeles','University of Chicago','University of Connecticut','University of Kansas Natural History Museum','University of Massachusetts Natural History Collec','University of Michigan Museum of Zoology','University of New Orleans','University of Texas, Austin Geological Sciences Te','University of Texas, Department of Anthropology','University of Washington','UTA Amphibian and Reptile Diversity Research Cente','Utah Museum of Natural History','Western Australia Museum','Yale Peabody Museum','Zoological Museum of Amsterdam','Zoologisches Museum Berlin'
