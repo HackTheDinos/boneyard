0e998c770762f3af4c6a8f7e0997edaab3c56c26
Jan Rubio <jan.rubio@opower.com>
2015-11-22 05:31:23 -0500

    Styling home page

--- app/assets/javascripts/dashboard/controllers/dashboardCtrl.js
+++ app/assets/javascripts/dashboard/controllers/dashboardCtrl.js
@@ -1,16 +1,52 @@
 angular.module('boneyard')
-    .controller('dashboardCtrl', function($scope, SpecimenService) {
+    .controller('dashboardCtrl', function($scope, $interval, $timeout, SpecimenService) {
         'use strict';
 
-        //SpecimenService
-        //    .all()
-        //    .then(function(specimens) {
-        //        $scope.specimens = specimens;
-        //    });
+        var LOADER_DOTS = 7;
+        var DOT_LOAD_DELAY = 200;
+        var DATA_LOAD_DELAY_OFFSET = 100;
+        var LOAD_DELAY = (LOADER_DOTS + 3) * DOT_LOAD_DELAY;
+        var dotIndex = 0;
+        var dotLoader;
 
-        $scope.search = function() {
+        $scope.loadData = false;
+        $scope.loaderDots = _.times(LOADER_DOTS, function() {
+            return false;
+        });
 
+        $scope.filter = function() {
+            $('.search-form--advanced').fadeOut(100);
         };
+
+        $('body').on('click', function() {
+            $('.search-form--advanced').fadeOut(100);
+        });
+        $('body').on('click', '.search-form--advanced', function() {
+            return false;
+        });
+        $('body').on('click', '.search-form--advanced .checkbox', function() {
+            $('#has-render').prop('checked', !$('#has-render').prop('checked'));
+            return false;
+        });
+        $('body').on('click', '.search-form--advanced .control-check-label', function() {
+            $('#has-render').prop('checked', !$('#has-render').prop('checked'));
+            return false;
+        });
+        $('body').on('click', '.search-form--advanced-toggle', function() {
+            $('.search-form--advanced').fadeIn(100);
+            return false;
+        });
+
+        function loadDots() {
+            dotLoader = $interval(function() {
+                $scope.loaderDots[dotIndex] = true;
+                dotIndex++;
+
+                if (dotIndex >= $scope.loaderDots.length) {
+                    $interval.cancel(dotLoader);
+                }
+            }, DOT_LOAD_DELAY);
+        }
 
         $scope.images = [];
 
@@ -23,6 +59,16 @@ angular.module('boneyard')
 
         function initialize() {
             $scope.specimens = [{}];
+
+            $timeout(function() {
+                $scope.isLoaded = true;
+            }, LOAD_DELAY);
+
+            $timeout(function() {
+                $scope.loadData = true;
+            }, LOAD_DELAY + DATA_LOAD_DELAY_OFFSET);
+
+            loadDots();
         }
 
         initialize();
