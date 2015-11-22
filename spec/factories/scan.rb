4c850a3be99c1e599d659be843c2fa4ca2583f04
Tony <tony@cc.com>
2015-11-22 02:53:40 -0500

    adding factory girl

---
+++ spec/factories/scan.rb
@@ -0,0 +1,6 @@
+# This will guess the User class
+FactoryGirl.define do
+  factory :scan do
+
+  end
+end
---
+++ spec/factories/specimen.rb
@@ -0,0 +1,6 @@
+# This will guess the User class
+FactoryGirl.define do
+  factory :specimen do
+
+  end
+end
